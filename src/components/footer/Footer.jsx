dbafe7022d86fb9a66daf90c04987c528489f3e6
Abeltosin <143393032+Abeltosin@users.noreply.github.com>
2024-03-14 15:45:43 +0100

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,41 +2,40 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className='container-fluid overflow-hidden' style={{background: 'rgba(0, 150, 255, 0.5)', minHeight: 400}}>
-      <div className="row row-cols-sm-2 justify-content-center" style={{padding: 30}}>
-        <div className="col col-md-6 col-lg-3">
-          <h3>OTA-MART</h3>
-          <p>Discover the unparalleled shopping experience awaiting you at Ota-Mart. With our vast array of handpicked products, exceptional customer service, and unbeatable prices, we're committed to redefining your online shopping journey. Join our community today and embark on a seamless voyage of convenience, quality, and savings.</p>
+    <div className="container-fluid bg-secondary" style={{minHeight: 400}}>
+      {/* first row */}
+      <div className="row row-cols-2 row-cols-sm-2 row-cols-md-2 row-cols-lg-4">
+        {/* first column */}
+        <div className="col">
+        <h3>OTA-MART</h3>
+      <p className='' style={{textAlign: 'justify', textJustify: 'inter-word'}}>Discover the unparalleled shopping experience awaiting you at Ota-Mart. With our vast array of handpicked products, exceptional customer service, and unbeatable prices, we're committed to redefining your online shopping journey. Join our community today and embark on a seamless voyage of convenience, quality, and savings.</p>
         </div>
-        <div className="col col-md-6 col-lg-3">
-          <h3 className=''>About Us</h3>
-          <ul className='list-group list-group-flush' style={{listStyle: 'none'}}>
-            <li className='list-group-item border-0' style={{background: 'rgba(0, 150, 255, 0.001)'}}>Integrity</li>
-            <li className='list-group-item border-0' style={{background: 'rgba(0, 150, 255, 0.001)'}}>Trust Worthy</li>
-            <li className='list-group-item border-0' style={{background: 'rgba(0, 150, 255, 0.001)'}}>Quality</li>
-            <li className='list-group-item border-0' style={{background: 'rgba(0, 150, 255, 0.001)'}}>Affordability</li>
-            <li className='list-group-item border-0' style={{background: 'rgba(0, 150, 255, 0.001)'}}>Unique</li>
-            <li className='list-group-item border-0' style={{background: 'rgba(0, 150, 255, 0.001)'}}>FAQ</li>
-            <li className='list-group-item border-0' style={{background: 'rgba(0, 150, 255, 0.001)'}}>Terms & Conditions</li>
-          </ul>
+        {/* second column */}
+        <div className="col">
+      <h3 className=''>About Us</h3>
+          <p><span className='text-danger'>&gt;</span>Intergrity</p>
+          <p><span className='text-danger'>&gt;</span>Trust Worthy</p>
+          <p><span className='text-danger'>&gt;</span>Quality</p>
+          <p><span className='text-danger'>&gt;</span>Affordability</p>
+          <p><span className='text-danger'>&gt;</span>Unique</p>
+          <p><span className='text-danger'>&gt;</span>FAQ</p>
+          <p><span className='text-danger'>&gt;</span>Terms & Conditions</p>
         </div>
-        <div className="col col-md-6 col-lg-3">
-          <h3 className=''>Customer Care</h3>
-          <ul className='list-group list-group-flush' style={{listStyle: 'none'}}>
-            <li className='list-group-item border-0' style={{background: 'rgba(0, 150, 255, 0.001)'}}>ota@help.hotline.com</li>
-            <li className='list-group-item border-0' style={{background: 'rgba(0, 150, 255, 0.001)'}}>07036897888</li>
-            <li className='list-group-item border-0' style={{background: 'rgba(0, 150, 255, 0.001)'}}>help@gmail.0ta</li>
-          </ul>
-        </div>
-        <div className="col col-md-6 col-lg-3">
-          <h3 className=''>Contact Us</h3>
-          <ul className='list-group list-group-flush' style={{listStyle: 'none'}}>
-            <li className='list-group-item border-0' style={{background: 'rgba(0, 150, 255, 0.001)'}}>07, Victory Estate, Isolo, LAgos, Nigeria</li>
-            <li className='list-group-item border-0' style={{background: 'rgba(0, 150, 255, 0.001)'}}>Phone: 07036897888</li>
-            <li className='list-group-item border-0' style={{background: 'rgba(0, 150, 255, 0.001)'}}>Email: help@gmail.0ta</li>
-          </ul>
-        </div>
+        {/* third column */}
+      <div className="col">
+      <h3 className=''>Customer Care</h3>
+        <p>ota@hotline.com</p>
+        <p>07036897888</p>
+        <p>help@gmail.0ta</p>
       </div>
+      {/* forth column */}
+        <div className="col overflow-hidden">
+      <h3 className=''>Contact Us</h3>
+      <p>07, Victory Estate, Isolo, LAgos, Nigeria</p>
+          <p>Phone: 07036897888</p>
+          <p>Email: help@gmail.com</p>
+        </div>
+    </div>
     </div>
   );
 }
