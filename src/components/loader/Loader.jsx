60ae5f2d0f7fcedad32c2bfb9947eb3dfd187cb2
Abeltosin <143393032+Abeltosin@users.noreply.github.com>
2024-03-30 12:34:52 +0100

    Update Loader.jsx

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,17 +1,21 @@
 import { RingLoader } from "react-spinners";
 // npm i react-spinners --save go to official website to check out more loading shapes
-import './loader.css'
+
 const Loader = () => {
   return (
-    <div className="loader">
-      <RingLoader
-        color="rgba(0, 210, 123, 0.7)"
-        loading
-        size={60}
-        speedMultiplier={1}
-        aria-label="Loading Spinner"
+   <div className="loader d-flex justify-content-center align-items-center vh-100">
+      <div className="row ">
+        <div className="col-12">
+          <RingLoader
+            color="rgba(0, 210, 123, 0.7)"
+            loading
+            size={60}
+            speedMultiplier={1}
+            aria-label="Loading Spinner"
             data-testid="loader"
-      />
+          />
+        </div>
+      </div>
     </div>
   );
 };
